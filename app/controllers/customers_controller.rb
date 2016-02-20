4d1398684f2db642de5f97e4d3ac8bc7d64f0456
Akihiko Yamagata <wishangel0126@gmail.com>
2016-02-20 18:00:33 +0900

    Create show method and edit validation

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -7,17 +7,30 @@ class CustomersController < ApplicationController
   end
 
   def create
+    @customer = Customer.new(customer_params)
+    if @customer.save
+      redirect_to @customer
+    else
+      render :new
+    end
   end
 
   def edit
+    @customer = Customer.find(params[:id])
   end
 
   def update
   end
 
   def show
+    @customer = Customer.find(params[:id])
   end
 
   def destroy
+  end
+  private
+
+  def customer_params
+    params.require(:customer).permit(:family_name,:given_name,:email)
   end
 end
