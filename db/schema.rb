7444a26537040d74760f7657420ce2618b5c6c19
Akihiko Yamagata <wishangel0126@gmail.com>
2016-02-21 14:58:02 +0900

    Create model and controller of comment

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160221045758) do
+ActiveRecord::Schema.define(version: 20160221055540) do
+
+  create_table "comments", force: :cascade do |t|
+    t.text     "body"
+    t.integer  "customer_id", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  add_index "comments", ["customer_id"], name: "index_comments_on_customer_id"
 
   create_table "companies", force: :cascade do |t|
     t.string   "name",       null: false
