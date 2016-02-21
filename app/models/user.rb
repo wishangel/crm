cb8f0a9657ee18eea27f18fc3996891cddc6a8ea
Akihiko Yamagata <wishangel0126@gmail.com>
2016-02-21 14:49:01 +0900

    Change login and register views

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  def full_name
+  	full_name = family_name + given_name
+  end
 end
